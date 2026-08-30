bdfe4d98e351b6033c9b79f0cceb868a82a647bd
Bjarki B. Harksen <bjarki@harksen.is>
2026-08-30 23:57:08 +0000

    init: initialize spring project

---
+++ src/main/java/is/hi/hbv501g/team2/playlist_management_service/PlaylistManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package is.hi.hbv501g.team2.playlist_management_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaylistManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaylistManagementServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/is/hi/hbv501g/team2/playlist_management_service/PlaylistManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package is.hi.hbv501g.team2.playlist_management_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaylistManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
