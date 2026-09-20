e5bd1f60e8da1627d8061c72d909368c4a1ca678
svartursandur <alexanderbjartur@gmail.com>
2026-09-20 22:21:48 +0000

    added entities (second try)

--- src/main/java/is/hi/hbv501g2026hteam2/playwright/persistence/entities/Track.java
+++ src/main/java/is/hi/hbv501g2026hteam2/playwright/persistence/entities/Track.java
@@ -2,20 +2,23 @@ package is.hi.hbv501g2026hteam2.playwright.persistence.entities;
 
 import is.hi.hbv501g2026hteam2.playwright.persistence.entities.TrackMetadata;
 import jakarta.persistence.Entity; 
+import jakarta.persistence.Id; 
+import jakarta.persistence.Embedded; 
+import jakarta.persistence.Transient; 
 import java.util.UUID;
 import java.nio.file.Path;
 
 @Entity
 public final class Track {
+    @Id
     private UUID id; 
     private UUID userId; 
-    private Path filepath; 
+    @Embedded
     private TrackMetadata metadata; 
 
-    public Track(UUID id, UUID userId, Path filepath, TrackMetadata metadata){
+    public Track(UUID userId, TrackMetadata metadata){
         this.id = UUID.randomUUID();
         this.userId = userId; 
-        this.filepath = filepath; 
         this.metadata = metadata; 
     }
 
@@ -25,10 +28,6 @@ public final class Track {
 
     public UUID getUserId(){
         return userId; 
-    }
-
-    public Path getFilePath(){
-        return filepath; 
     }
 
     public TrackMetadata getMetadata(){
--- src/main/java/is/hi/hbv501g2026hteam2/playwright/persistence/entities/TrackMetadata.java
+++ src/main/java/is/hi/hbv501g2026hteam2/playwright/persistence/entities/TrackMetadata.java
@@ -1,29 +1,29 @@
 package is.hi.hbv501g2026hteam2.playwright.persistence.entities;
 
-import jakarta.persistence.Entity; 
+import jakarta.persistence.Embeddable; 
+import jakarta.persistence.Transient; 
 import java.util.UUID;
 import java.nio.file.Path;
 import java.awt.Image; 
 import java.time.Duration; 
 import java.util.Date; 
 
+@Embeddable
 public final class TrackMetadata {
     private String title; 
     private String artist; 
     private String genre; 
     private String albumName; 
     private String comment; 
-    private Image albumArt; 
     private Duration duration; 
     private Date releaseDate; 
 
-    public TrackMetadata(String title, String artist, String genre, String albumName, Image albumArt, Date releaseDate, Duration duration, String comment){
+    public TrackMetadata(String title, String artist, String genre, String albumName, Date releaseDate, Duration duration, String comment){
         this.title = title; 
         this.artist = artist; 
         this.genre = genre; 
         this.albumName = albumName; 
         this.comment = comment; 
-        this.albumArt = albumArt; 
         this.duration = duration; 
         this.releaseDate = releaseDate; 
     }
@@ -42,10 +42,6 @@ public final class TrackMetadata {
 
     public String getAlbumName() {
         return albumName;
-    }
-
-    public Image getAlbumArt() {
-        return albumArt;
     }
 
     public Date getReleaseDate() {
