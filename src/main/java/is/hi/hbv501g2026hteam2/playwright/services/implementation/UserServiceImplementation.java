11f3f0eb507aac06c419b2d2638df617caf65744
Hermann <hfg8@hi.is>
2026-09-20 21:35:46 +0000

    Implement user registration

--- src/main/java/is/hi/hbv501g2026hteam2/playwright/services/implementation/UserServiceImplementation.java
+++ src/main/java/is/hi/hbv501g2026hteam2/playwright/services/implementation/UserServiceImplementation.java
@@ -1,47 +1,57 @@
 package is.hi.hbv501g2026hteam2.playwright.services.implementation;
 
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import is.hi.hbv501g2026hteam2.playwright.persistence.entities.User;
+import is.hi.hbv501g2026hteam2.playwright.persistence.repositories.UserRepository;
 import is.hi.hbv501g2026hteam2.playwright.services.UserService;
 
 @Service
 public class UserServiceImplementation implements UserService {
-    private UserRepository repository; 
+    private UserRepository repository;
 
     @Autowired
-    public UserServiceImplementation(UserRepository repository){
+    public UserServiceImplementation(UserRepository repository) {
         this.repository = repository;
     }
 
     @Override
-    public User register(User user){
-        return null; 
+    public User register(User user) {
+        return repository.save(user);
     }
 
     @Override
-    public void delete(User user){} 
-    
-    @Override
-    public void login(User user){} 
+    public void delete(User user) {
+    }
 
     @Override
-    public void logout(User user){} 
+    public void login(User user) {
+    }
 
     @Override
-    public User get(UUID id){
-        return null; 
-    } 
+    public void logout(User user) {
+    }
 
     @Override
-    public User getByEmail(String email){
-        return null; 
-    } 
-    
-    @Override
-    public User updateEmail(User user, String newEmail){
-        return null; 
-    } 
+    public User get(UUID id) {
+        return null;
+    }
 
     @Override
-    public User updatePassword(User user, String newPassword){
-        return null; 
+    public User getByEmail(String email) {
+        return null;
+    }
+
+    @Override
+    public User updateEmail(User user, String newEmail) {
+        return null;
+    }
+
+    @Override
+    public User updatePassword(User user, String newPassword) {
+        return null;
     }
 }
